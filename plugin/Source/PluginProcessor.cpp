fbe4a603c5964b77031e021c23bffcd3f9c48072
Roland Rabien <figbug@gmail.com>
2020-11-22 07:10:11 -0800

    Fix up params

--- plugin/Source/PluginProcessor.cpp
+++ plugin/Source/PluginProcessor.cpp
@@ -10,39 +10,39 @@ MverbAudioProcessor::MverbAudioProcessor()
 {
 	mix           = addExtParam ("MIX",           "Mix",     "", "%",	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v, 2);
+		return formatNumber (v);
 	});
-	predelay      = addExtParam ("PREDELAY",      "Pre",     "", "ms", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  50.0f, 0.0f, [] (const gin::Parameter&, float v)
+	predelay      = addExtParam ("PREDELAY",      "Pre delay",     "Pre", "ms", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  50.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v * 2, 2);
+		return formatNumber (v * 2);
 	});
-	earlymix      = addExtParam ("EARLYMIX",      "EL/Mx",   "", "%",  	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
+	earlymix      = addExtParam ("EARLYMIX",      "Early late mix",   "EL Mx", "%",  	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v, 2);
+		return formatNumber (v);
 	});
-	size          = addExtParam ("SIZE",          "Size",    "", "%",  	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
+	size          = addExtParam ("SIZE",          "Size",    "", "",  	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String ((0.95f * v / 100.0f) + 0.05f, 2);
+		return formatNumber ((0.95f * v / 100.0f) + 0.05f);
 	});
 	density       = addExtParam ("DENSITY",       "Density", "", "",  	{ 0.0f, 100.0f, 0.0f, 1.0f },  50.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v / 100.0f * 0.7995f + 0.005f, 2);
+		return formatNumber (v / 100.0f * 0.7995f + 0.005f);
 	});
-	bandwidthfreq = addExtParam ("BANDWIDTHFREQ", "Band",    "", "Hz", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  90.0f, 0.0f, [] (const gin::Parameter&, float v)
+	bandwidthfreq = addExtParam ("BANDWIDTHFREQ", "Bandwidth Frequency",    "Band", "Hz", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  90.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v / 100.0f * 18400.0f + 100.0f, 2);
+		return formatNumber (v / 100.0f * 18400.0f + 100.0f);
 	});
 	decay         = addExtParam ("DECAY",         "Decay",   "", "",  	{ 0.0f, 100.0f, 0.0f, 1.0f },  50.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (v / 100.0f * 0.7995f + 0.005f, 2);
+		return formatNumber (v / 100.0f * 0.7995f + 0.005f);
 	});
-	dampingfreq   = addExtParam ("DAMPINGFREQ",   "Damp",    "", "Hz", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  90.0f, 0.0f, [] (const gin::Parameter&, float v)
+	dampingfreq   = addExtParam ("DAMPINGFREQ",   "Damping Frequency",    "Damp", "Hz", 	{ 0.0f, 100.0f, 0.0f, 1.0f },  90.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String ((100.0f - v) / 100.0f * 18400.0f + 100.0f, 2);
+		return formatNumber ((100.0f - v) / 100.0f * 18400.0f + 100.0f);
 	});
 	gain          = addExtParam ("GAIN",          "Gain",    "", "dB",  	{ 0.0f, 100.0f, 0.0f, 1.0f }, 100.0f, 0.0f, [] (const gin::Parameter&, float v)
 	{
-		return juce::String (Decibels::gainToDecibels (v / 100.0f), 2);
+		return formatNumber (Decibels::gainToDecibels (v / 100.0f));
 	});
 
 	for (int i = 0; i < BinaryData::namedResourceListSize; i++)
